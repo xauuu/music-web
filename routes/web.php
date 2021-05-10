e741218d0bea7ab7d720b9bb4683d710701f154c
Tran Quang Dat <qdatqb@gmail.com>
2021-05-10 16:57:07 +0700

    change routes

--- routes/web.php
+++ routes/web.php
@@ -19,12 +19,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('admin');
 });
-Route::prefix('admin')->group(function () {
-    Route::get('add-album', [AlbumController::class, 'add_album']);
-    Route::post('save-album', [AlbumController::class, 'save_album']);
-    Route::get('all-album', [AlbumController::class, 'all_album']);
+Route::get('add-album', [AlbumController::class, 'add_album']);
+Route::post('save-album', [AlbumController::class, 'save_album']);
+Route::get('all-album', [AlbumController::class, 'all_album']);
 
-    Route::get('add-song', [SongController::class, 'add_song']);
-    Route::post('save-song', [SongController::class, 'save_song']);
-    Route::get('all-song', [SongController::class, 'all_song']);
-});
+Route::get('add-song', [SongController::class, 'add_song']);
+Route::post('save-song', [SongController::class, 'save_song']);
+Route::get('all-song', [SongController::class, 'all_song']);
