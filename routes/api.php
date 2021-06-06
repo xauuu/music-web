52d08a2a9ef9640be9810945ac2c62bff6e3fa3b
Quang Đạt <74817749+xauuu@users.noreply.github.com>
2021-06-06 08:23:52 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\api\FavoriteController;
+use App\Http\Controllers\Api\FavoriteController;
 use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
